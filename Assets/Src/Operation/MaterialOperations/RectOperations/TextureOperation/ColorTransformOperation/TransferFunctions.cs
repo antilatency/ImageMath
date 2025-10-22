56575a390f58c125a088921ff0c1dd09b0a0ee25
Pavel Kretov <pavel.kretov@antilatency.com>
2025-10-22 19:34:48 +0300

    TransferFunctions.cs: add ARRI LogC3

--- Assets/Src/Operation/MaterialOperations/RectOperations/TextureOperation/ColorTransformOperation/TransferFunctions.cs
+++ Assets/Src/Operation/MaterialOperations/RectOperations/TextureOperation/ColorTransformOperation/TransferFunctions.cs
@@ -31,5 +31,39 @@ namespace ImageMath {
             // TODO: get rid of this cast when covariant overrides are available.
             return (UnpackPiecewiseLinearLog)(PackBlackmagicDesignFilmGen5().CreateInverse(texture));
         }
+
+        public static PackPiecewiseLinearLog PackArriLogC3(Texture? texture = null) {
+
+            // https://www.arri.com/resource/blob/31918/66f56e6abb6e5b6553929edf9aa7483e/2017-03-alexa-logc-curve-in-vfx-data.pdf
+            // ALEXA Log C Curve: Usage in VFX, by Harald Brendel, 09-Mar-17
+
+            // Constants from the spec with their original names, for the "default" curve (EI 800). Search
+            // the document for "Use the parameters in the following table for conversion between Log C values
+            // and exposure values" and refer to the table row with the EI value of 800.
+            const double cut = 0.010591;
+            const double a = 5.555556;
+            const double b = 0.052272;
+            const double c = 0.247190;
+            const double d = 0.385537;
+            const double e = 5.367655;
+            const double f = 0.092809;
+
+            const double ln10 = 2.3025850929940456840179914546844;
+
+            return new PackPiecewiseLinearLog(texture) {
+                Threshold = (float)cut,
+                LinearScale = (float)e,
+                LinearOffset = (float)f,
+                LogInnerScale = (float)a,
+                LogInnerOffset = (float)b,
+                LogOuterScale = (float)(c / ln10),
+                LogOuterOffset = (float)d,
+            };
+        }
+
+        public static UnpackPiecewiseLinearLog UnpackArriLogC3(Texture? texture = null) {
+            // TODO: get rid of this cast when covariant overrides are available.
+            return (UnpackPiecewiseLinearLog)(PackArriLogC3().CreateInverse(texture));
+        }
     }
 }
