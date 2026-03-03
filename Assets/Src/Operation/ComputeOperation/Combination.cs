88e14afd8e79d8d0e9ab0cfd8f28c0a61a40b938
Sergei Nosov <sergei.nosov@atilatency.com>
2026-03-03 20:06:44 +0200

    fix combination namespace

--- Assets/Src/Operation/ComputeOperation/Combination.cs
+++ Assets/Src/Operation/ComputeOperation/Combination.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ImageMath.Operations
+namespace ImageMath
 {
     public record Combination : IEnumerable
     {
